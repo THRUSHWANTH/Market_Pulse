bd321d15ad1a1f100bfa9eb8d00b524891abedab
Thrushwanth Kakuturu <69004763+THRUSHWANTH@users.noreply.github.com>
2025-03-14 12:27:11 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,30 +1,3 @@
-# import streamlit as st
-
-# # Fix sidebar height using CSS
-# st.markdown(
-#     """
-#     <style>
-#     [data-testid="stSidebarContent"] {
-#         height: 100vh !important;  /* Ensures sidebar fills the screen height */
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
-
-# st.title("📈 Yahoo Finance Data Pull")
-
-# st.write("""
-# Welcome to the Yahoo Finance Data Pull application. Use the navigation menu to explore:
-
-# - **Stock Company Info**: Retrieve information about specific companies.
-# - **Market Index Info**: View details of major market indices.
-# - **Market Screeners**: Discover top gainers, losers, and more.
-# """)
-
-# # Display image using the correct parameter
-# st.sidebar.image("img.png", use_container_width=True)
-
 import streamlit as st
 from yahooquery import Ticker, Screener
 import pandas as pd
@@ -46,11 +19,11 @@ st.title("MarketPulse 📈")
 st.write("""
 Welcome to the Yahoo Finance Data Pull application. Use the navigation menu to explore:
 
-- **Stock Company Info**: Retrieve information about specific companies.
-- **Market Index Info**: View details of major market indices.
 - **Market Screeners**: Discover top gainers, losers, and more.
+- **Market Index Info**: View details of major market indices.
 - **Market Summary**: Get an overview of the US market.
 - **Trending Stocks**: View the latest trending stocks.
+- **Stock Company Info**: Retrieve information about specific companies.
 """)
 
 # Display image using the correct parameter
